93e276540aea3e9f0e2a121e57f90f7e808a35e2
Gabriel Curti <gabrielcurti.dev@outlook.com>
2025-02-19 21:38:08 -0300

    copiar email

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuBurguer = document.querySelector('.menu-burguer')
 const menuMobile = document.querySelector('.menu-mobile')
 const mobileTxt = document.querySelectorAll(".mobile-txt")
+const btnEmail = document.querySelector('.btn-email')
 
 function menuCliquei(){
     menuMobile.classList.toggle('open')
@@ -12,3 +13,17 @@ function remove(){
     menuMobile.classList.remove('open')
     document.body.style.overflow = "";
 }
+
+btnEmail.addEventListener('click', (copiarTexto) =>{
+    navigator.clipboard.writeText(btnEmail.value)
+    .then(() => {
+        let mensagem = document.getElementById("mensagem");
+        mensagem.style.display = "block";
+        setTimeout(() => mensagem.style.display = "none", 1500);
+    })
+    .catch(err => {
+        console.error("Erro ao copiar: ", err);
+    });
+
+    
+})
