86b6bf9866262ce9d0517c5f91ddf0d31e8655c8
Gabriel Curti <gabrielcurti.dev@outlook.com>
2025-02-23 15:51:45 -0300

    baixar cv

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const mobileTxt = document.querySelectorAll(".mobile-txt")
 const btnEmail = document.querySelector('.btn-email')
 const navMenu = document.querySelectorAll('.nav-menu li')
 
+
+//MENU MOBILE ADD E REMOVE
+
 function menuCliquei(){
     menuMobile.classList.toggle('open')
     
@@ -20,21 +23,42 @@ function remove(){
     document.body.style.overflow = "";
 }
 
-btnEmail.addEventListener('click', (copiarTexto) =>{
-    let email = 'gabrielcurti.dev@outlook.com' 
+//====================================
 
-    navigator.clipboard.writeText(email)
-    .then(() => {
-        let mensagem = document.getElementById("mensagem");
-        mensagem.style.display = "block";
-        setTimeout(() => mensagem.style.display = "none", 1500);
-    })
-    .catch(err => {
-        console.error("Erro ao copiar: ", err);
-    });
+//BAIXAR CURRICULO
+
+const btnCurriculo = document.getElementById('download-cv')
+.addEventListener('click', function(){
+    let mensagem = document.getElementById('mensagem')
+    mensagem.style.display = 'block'
+    setTimeout(() => {
+        mensagem.style.display = 'none'
+    }, 2000)
+})
+
+//====================================
+
+// COPIAR EMAIL E SOLTAR A MSG DE COPIADO
+
+// btnEmail.addEventListener('click', (copiarTexto) =>{
+//     let email = 'gabrielcurti.dev@outlook.com' 
+
+//     navigator.clipboard.writeText(email)
+//     .then(() => {
+//         let mensagem = document.getElementById("mensagem");
+//         mensagem.style.display = "block";
+//         setTimeout(() => mensagem.style.display = "none", 1500);
+//     })
+//     .catch(err => {
+//         console.error("Erro ao copiar: ", err);
+//     });
 
     
-})
+// })
+
+//====================================
+
+//COLOCAR UMA MARCAÇÃO  FIXA DE BORDER BUTTON NO MENU E IR TRANSFERINDO AO TROCAR DE LINK
 
 // navMenu[0].classList.add('active')
 
